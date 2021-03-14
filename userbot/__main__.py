bc4da353e62540953329ffdf4bedd61f7a0681e5
lyuzi09 <80038946+lyuzi09@users.noreply.github.com>
2021-03-14 14:18:38 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Lord-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"RENZY-USERBOT Version {BOT_VER} [Succesfully Activated!]")
 
 
 if len(argv) not in (1, 3, 4):
