a2e6d70028e0c5db577775b81f32cf2d2d3f9a33
lyuzi09 <80038946+lyuzi09@users.noreply.github.com>
2021-03-14 21:06:24 +0700

    Update kekuatan.py

--- userbot/modules/kekuatan.py
+++ userbot/modules/kekuatan.py
@@ -8,7 +8,6 @@ from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
 @register(outgoing=True, pattern=r'^.kekuatan(:? |$)([1-8])?')
 async def _(fry):
     await fry.edit("`Bismillah`")
-    sleep(2)
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
         return
