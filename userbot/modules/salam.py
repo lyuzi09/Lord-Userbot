cd0d7f38f6702ebbcf4dd00d35b51c574ae35bec
lyuzi09 <80038946+lyuzi09@users.noreply.github.com>
2021-03-14 13:10:46 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -10,26 +10,36 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^.P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Hallo Perkenalkan nama saya Renzy`")
-    await typew.edit("`Assalamu'alaikum`")
+    sleep(1)
+    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER}**")
+    sleep(2)
+    await typew.edit("`Assalamu'alaikum...`")
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Hallo Perkenalkan nama saya Renzy`")
-    await typew.edit("`Assalamu'alaikum`")
+    sleep(1)
+    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER}**")
+    sleep(2)
+    await typew.edit("`Assalamu'alaikum...`")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
+    sleep(2)
     await typew.edit("`Wa'alaikumussalam`")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
+    sleep(2)
     await typew.edit("`Wa'alaikumussalam`")
 
 
